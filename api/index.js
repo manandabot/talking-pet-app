a07dd6b7fe1b9b5934a5eb358f48448429075fcc
manandabot <metre_yeasty_10@icloud.com>
2025-07-13 15:32:02 +0100

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  // Enable CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
+  if (req.method === 'GET') {
+    return res.json({ message: 'API is working!' });
+  }
+  
+  return res.status(405).json({ error: 'Method not allowed' });
+}
